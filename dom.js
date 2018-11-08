c5b83644bc07e54d744f2cdf6f35defd4e5e52e3
jcharnley <jasoncharnley@gmail.com>
2018-11-08 18:26:32 +0000

    add item to list

--- dom.js
+++ dom.js
@@ -1,28 +1,31 @@
-
 // part 2 linking it all together
 // The function here is called an iife,
 // it keeps everything inside hidden from the rest of our application
 (function() {
   // This is the dom node where we will keep our todo
-  var container = document.getElementById('todo-container');
-  var addTodoForm = document.getElementById('add-todo');
+  var container = document.getElementById("todo-container");
+  var addTodoForm = document.getElementById("add-todo");
 
   var state = [
-    { id: -3, description: 'first todo' },
-    { id: -2, description: 'second todo' },
-    { id: -1, description: 'third todo' },
+    { id: -3, description: "first todo" },
+    { id: -2, description: "second todo" },
+    { id: -1, description: "third todo" }
   ]; // this is our initial todoList
 
   // This function takes a todo, it returns the DOM node representing that todo
   var createTodoNode = function(todo) {
-    var todoNode = document.createElement('li');
-    // you will need to use addEventListener
+    var todoNode = document.createElement("li");
 
+    var listSpan = document.createElement("span");
+    todoNode.appendChild(listSpan);
+    listSpan.textContent = todo.description;
+
+    console.log(todoNode);
     // add span holding description
 
     // this adds the delete button
-    var deleteButtonNode = document.createElement('button');
-    deleteButtonNode.addEventListener('click', function(event) {
+    var deleteButtonNode = document.createElement("button");
+    deleteButtonNode.addEventListener("click", function(event) {
       var newState = todoFunctions.deleteTodo(state, todo.id);
       update(newState);
     });
@@ -37,15 +40,20 @@
 
   // bind create todo form
   if (addTodoForm) {
-    addTodoForm.addEventListener('submit', function(event) {
-      // https://developer.mozilla.org/en-US/docs/Web/Events/submit
-      // what does event.preventDefault do?
-      // what is inside event.target?
+    addTodoForm.addEventListener("submit", function(event) {
+      event.preventDefault();
 
-      var description = '?'; // event.target ....
+      let newTodo = todoFunctions.createTodo(
+        document.querySelector(".inputText").value
+      );
+      console.log(newTodo);
 
+      var description = newTodo.description; // event.target ....
+
+      console.log(description);
       // hint: todoFunctions.addTodo
-      var newState = []; // ?? change this!
+      var newState = todoFunctions.addTodo(state, description); // ?? change this!
+
       update(newState);
     });
   }
@@ -58,7 +66,7 @@
 
   // you do not need to change this function
   var renderState = function(state) {
-    var todoListNode = document.createElement('ul');
+    var todoListNode = document.createElement("ul");
 
     state.forEach(function(todo) {
       todoListNode.appendChild(createTodoNode(todo));
