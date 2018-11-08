8c51251e3475dc777acb83fe131ba33a1ae58483
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-11-08 16:50:52 +0000

    Add mark to do button using existing delete button as a template '

--- dom.js
+++ dom.js
@@ -1,35 +1,39 @@
-
 // part 2 linking it all together
 // The function here is called an iife,
 // it keeps everything inside hidden from the rest of our application
 (function() {
   // This is the dom node where we will keep our todo
-  var container = document.getElementById('todo-container');
-  var addTodoForm = document.getElementById('add-todo');
+  var container = document.getElementById("todo-container");
+  var addTodoForm = document.getElementById("add-todo");
 
   var state = [
-    { id: -3, description: 'first todo' },
-    { id: -2, description: 'second todo' },
-    { id: -1, description: 'third todo' },
+    { id: -3, description: "first todo" },
+    { id: -2, description: "second todo" },
+    { id: -1, description: "third todo" }
   ]; // this is our initial todoList
 
   // This function takes a todo, it returns the DOM node representing that todo
   var createTodoNode = function(todo) {
-    var todoNode = document.createElement('li');
+    var todoNode = document.createElement("li");
     // you will need to use addEventListener
 
     // add span holding description
 
     // this adds the delete button
-    var deleteButtonNode = document.createElement('button');
-    deleteButtonNode.addEventListener('click', function(event) {
+    var deleteButtonNode = document.createElement("button");
+    deleteButtonNode.addEventListener("click", function(event) {
       var newState = todoFunctions.deleteTodo(state, todo.id);
       update(newState);
     });
     todoNode.appendChild(deleteButtonNode);
 
     // add markTodo button
-
+    var markButtonNode = document.createElement("button");
+    markButtonNode.addEventListener("click", function(event) {
+      var newState = todoFunctions.markTodo(state, todo.id);
+      update(newState);
+    });
+    todoNode.appendChild(markButtonNode);
     // add classes for css
 
     return todoNode;
@@ -37,12 +41,12 @@
 
   // bind create todo form
   if (addTodoForm) {
-    addTodoForm.addEventListener('submit', function(event) {
+    addTodoForm.addEventListener("submit", function(event) {
       // https://developer.mozilla.org/en-US/docs/Web/Events/submit
       // what does event.preventDefault do?
       // what is inside event.target?
 
-      var description = '?'; // event.target ....
+      var description = "?"; // event.target ....
 
       // hint: todoFunctions.addTodo
       var newState = []; // ?? change this!
@@ -58,7 +62,7 @@
 
   // you do not need to change this function
   var renderState = function(state) {
-    var todoListNode = document.createElement('ul');
+    var todoListNode = document.createElement("ul");
 
     state.forEach(function(todo) {
       todoListNode.appendChild(createTodoNode(todo));
